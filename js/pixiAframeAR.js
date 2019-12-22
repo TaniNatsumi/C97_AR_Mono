a6edf8c8bee179b506ca1964d27e0a2a7e867ed5
TaniNatsumi <58015504+TaniNatsumi@users.noreply.github.com>
2019-12-22 16:14:09 +0900

    Add files via upload

--- js/pixiAframeAR.js
+++ js/pixiAframeAR.js
@@ -144,7 +144,7 @@ window.onload = function () {
 		plane.setAttribute('height', '5');
 		plane.setAttribute('width', '5');
 		//�}�[�J�[����ɂ������f���̑��Έʒu
-		plane.setAttribute('position', '1.4 1 0')
+		plane.setAttribute('position', '1.4 2.5 0')
 		var stand = stand_mode ? '0 0 0' : '-90 0 0';
 		plane.setAttribute('rotation', stand);
 		marker.appendChild(plane);
